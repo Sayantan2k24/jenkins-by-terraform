9422371eab4a0803ec933f3aee86d74887b04138
Sayantan Samanta <90416470+Sayantan2k24@users.noreply.github.com>
2025-01-06 18:30:58 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,5 +1,6 @@
 sudo yum update -y
-sudo amazon-linux-extras install java-openjdk11 -y
+wget https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.rpm
+sudo yum -y install https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.rpm
 java --version
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
